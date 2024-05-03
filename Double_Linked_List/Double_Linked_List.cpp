d3120701b04d07a02c01fc6e73cc0d71802a1802
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-05-03 13:07:07 +0700

    menentukan data nama mahasiswa pada node baru

--- Double_Linked_List/Double_Linked_List.cpp
+++ Double_Linked_List/Double_Linked_List.cpp
@@ -16,6 +16,10 @@ void addNode() {
 	Node* newNode = new Node();  //step 1: create a new mode
 	cout << "\nEnter the roll number of the student: ";
 	cin >> newNode->noMhs;       //assign value to the data field of the new node
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name;        //assign value to the data field of the new node
+
+
 	
 }
 
