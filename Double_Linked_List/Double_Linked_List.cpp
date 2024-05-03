3e119ff7f7152a9830495875ff80c1de86c57235
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-05-03 13:18:02 +0700

    membuat point node baru ke start

--- Double_Linked_List/Double_Linked_List.cpp
+++ Double_Linked_List/Double_Linked_List.cpp
@@ -27,6 +27,9 @@ void addNode() {
 			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
 			return;
 		}
+		// if the list is empty, make the new node the START
+		newNode->next = START;        //step 3: make the new node point to the first node
+	
 	}
 }
 
